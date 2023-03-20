78e5febc34e3e6e8c5e19ef7e966f8892c7e3f2b
Mustafa Aytemür <maytemur@gmail.com>
2023-03-20 16:27:38 +0300

    Initial Commit

---
+++ 07InternetIslemleri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  07InternetIslemleri
+//
+//  Created by maytemur on 20.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
